0859fee3e61debc120b15b2543331b8aadbee797
satriaranggaj <satriarangga43@gmail.com>
2023-07-26 16:35:35 +0700

    created feedback form

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -18,5 +18,31 @@ window.onscroll = function(){
       });
 };
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyuqtPPjFO-zXGSp1JRUhtDHuC43HdpaAVgNjsAAPHWGFOjgjiAqk5U4IEnjf9Q37QL/exec';
+const form = document.forms['submit-to-google-sheet']
+const btnSubmit = document.querySelector('.btn-submit');
+const btnLoading = document.querySelector('.btn-loading');
+const alert = document.querySelector('.alert');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // ketika tombol submit di klik
+  // tampilkan tombol loading, hilangkan tombol submit
+  btnLoading.classList.toggle('d-none');
+  btnSubmit.classList.toggle('d-none');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      // tampilkan tombol submit, hilangkan tombol loading
+      btnLoading.classList.toggle('d-none');
+      btnSubmit.classList.toggle('d-none');
+      // tampilkan alert
+      alert.classList.toggle('d-none');
+      // reset form
+      form.reset();
+      console.log('Success!', response)
+    })
+    .catch(error => console.error('Error!', error.message))
+  })
+
 
 
